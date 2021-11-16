0a92a8edfb69d0e6172c4c2384f0d8734b4f611f
251548 <93525407+251548@users.noreply.github.com>
2021-11-16 16:38:38 +0100

    feat: add contact page

--- project_xplora/frontend/src/App.js
+++ project_xplora/frontend/src/App.js
@@ -1,32 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import Homepage from "./features/homepage/Home";
 import Login from "./features/Login/Login";
+import Contact from "./features/contact/Contact";
 // import ReactFullpage from '@fullpage/react-fullpage-umd'; // will return static version on server and "live" version on client
 
 function App() {
+  const [activeTab, setActiveTab] = useState({
+    logo: "logo",
+    home: "active",
+    feature: "",
+    login: "",
+    contact: "",
+  });
+
+  const allEmptyState = {
+    logo: "logo",
+    home: "",
+    feature: "",
+    login: "",
+    contact: "",
+  };
+
+  const toggleClassName = (e) => {
+    switch (e.target.id) {
+      case "home":
+        setActiveTab({ ...allEmptyState, home: "active" });
+        break;
+      case "feature":
+        setActiveTab({ ...allEmptyState, feature: "active" });
+        break;
+
+      case "login":
+        setActiveTab({ ...allEmptyState, login: "active" });
+        break;
+      case "contact":
+        setActiveTab({ ...allEmptyState, contact: "active" });
+        break;
+
+      default:
+        break;
+    }
+    setActiveTab.placeholder = "active";
+  };
+
   return (
     <div className="App">
       <header>
-        <a href="." className="logo">
+        <a href="." id="logo" className={activeTab.logo}>
           X-plora
         </a>
         <ul>
           <li>
-            <a href="." className="active">
+            <a
+              href="."
+              id="home"
+              className={activeTab.home}
+              onClick={toggleClassName}
+            >
               Home
             </a>
           </li>
           <li>
-            <a href="#next">Featues</a>
+            <a
+              href="#next"
+              id="feature"
+              className={activeTab.feature}
+              onClick={toggleClassName}
+            >
+              Featues
+            </a>
           </li>
           <li>
-            <a href="#Login">Login/Register</a>
+            <a
+              href="#Login"
+              id="login"
+              className={activeTab.login}
+              onClick={toggleClassName}
+            >
+              Login/Register
+            </a>
           </li>
           <li>
-            <a href=".">Register</a>
+            <a
+              href="#Contact"
+              id="contact"
+              className={activeTab.contact}
+              onClick={toggleClassName}
+            >
+              Contact
+            </a>
           </li>
         </ul>
       </header>
@@ -38,8 +103,11 @@ function App() {
       <section className="Login">
         <Login />
       </section>
+
+      <section className="Contact">
+        <Contact />
+      </section>
     </div>
   );
 }
-
 export default App;
---
+++ project_xplora/frontend/src/features/contact/Contact.js
@@ -0,0 +1,54 @@
+import "./Contact.css";
+import { useEffect } from "react";
+import Phone from "./phone.png";
+import Phone2 from "./phone2.png";
+
+import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
+import * as ScrollMagic from "scrollmagic";
+import { TweenMax, TimelineMax, set } from "gsap";
+import STARS from "./stars.png";
+// ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
+
+export default function Contact() {
+  //   useEffect(() => {
+  //     // ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
+
+  //     let controller = new ScrollMagic.Controller();
+  //     let timeline = new TimelineMax();
+
+  //     timeline.to("#Phone1", 3, { y: 1000 });
+  //     //   .to("#Phone3", 100, { y: "-11000px" }, "-=100")
+  //     //   .to("#Phone2", 100, { y: "-8000px" }, "-=110");
+
+  //     let scene = new ScrollMagic.Scene({
+  //       triggerElement: "#workd",
+  //       duration: "100%",
+  //       triggerHook: 0,
+  //     })
+  //       .setTween(timeline)
+  //       .addTo(controller);
+  //   }, []);
+
+  return (
+    <section className="ContactMain" id="Contact">
+      <img src={Phone2} id="Phone1" />
+      <img src={Phone} id="Phone2" />
+      <img src={Phone} id="Phone3" />
+      <img src={Phone2} id="Phone4" />
+      <img src={Phone2} id="Phone5" />
+      <img src={STARS} id="stars" />
+
+      <div className="contactContainer">
+        <div className="phone">
+          <img src="https://img.icons8.com/external-flatart-icons-lineal-color-flatarticons/64/000000/external-phone-contact-us-flatart-icons-lineal-color-flatarticons.png" />
+          <h2 id="phone">+48 729636533</h2>
+        </div>
+
+        <div className="email">
+          <img src="https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/64/000000/external-email-advertising-kiranshastry-lineal-color-kiranshastry-3.png" />
+          <h2 id="phone">contact@xplora.com</h2>
+        </div>
+      </div>
+    </section>
+  );
+}
